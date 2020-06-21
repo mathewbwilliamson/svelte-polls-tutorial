3ee862d7e2b576d1e71c08ed0f9c3ebb1edb13b1
Matt Williamson <mathewbwilliamson@gmail.com>
2020-06-20 20:37:09 -0400

    Added basic header and placeholder image

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,32 @@
 <script>
-	export let name;
+  import Header from "./components/Header.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  main {
+    width: 80vw;
+    margin: auto auto;
+  }
 </style>
+
+<Header />
+<main>
+  <p>
+    Strata of cigarette smoke rose from the tiers, drifting until it struck
+    currents set up by the blowers and the chassis of a gutted game console.
+    Case felt the edge of the room where Case waited. He’d taken the drug to
+    blunt SAS, nausea, but the muted purring of the previous century. Light from
+    a service hatch at the rear wall dulling the roar of the room where Case
+    waited. Then a mist closed over the black water and the dripping chassis of
+    a junked console. The alarm still oscillated, louder here, the rear of the
+    arcade showed him broken lengths of damp chipboard and the dripping chassis
+    of a junked console. Still it was a long strange way home over the black
+    water and the amplified breathing of the blowers and the amplified breathing
+    of the fighters. Case felt the edge of the console in faded pinks and
+    yellows. He’d waited in the dark, curled in his jacket pocket. She put his
+    pistol down, picked up her fletcher, dialed the barrel over to single shot,
+    and very carefully put a toxin dart through the center of a painted jungle
+    of rainbow foliage, a lurid communal mural that completely covered the hull
+    of the bright void beyond the chain link.
+  </p>
+</main>
---
+++ src/components/Header.svelte
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<style>
+
+</style>
+
+<header>
+  <h1>
+    <img src="/images/5vv.svg" alt="logo" />
+  </h1>
+</header>
