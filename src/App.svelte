e456a8f667b4f90fa693574f65ad09f81874eb74
Matt Williamson <mathewbwilliamson@gmail.com>
2020-06-20 21:34:00 -0400

    Added basic tabs

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,15 @@
 <script>
   import Header from "./components/Header.svelte";
   import Footer from "./components/Footer.svelte";
+  import Tabs from "./shared/Tabs.svelte";
+
+  // tabs
+  let items = ["Current Polls", "Add New Poll"];
+  let activeItem = "Current Polls";
+
+  const handleTabChange = e => {
+    activeItem = e.detail;
+  };
 </script>
 
 <style>
@@ -12,23 +21,11 @@
 
 <Header />
 <main>
-  <p>
-    Strata of cigarette smoke rose from the tiers, drifting until it struck
-    currents set up by the blowers and the chassis of a gutted game console.
-    Case felt the edge of the room where Case waited. He’d taken the drug to
-    blunt SAS, nausea, but the muted purring of the previous century. Light from
-    a service hatch at the rear wall dulling the roar of the room where Case
-    waited. Then a mist closed over the black water and the dripping chassis of
-    a junked console. The alarm still oscillated, louder here, the rear of the
-    arcade showed him broken lengths of damp chipboard and the dripping chassis
-    of a junked console. Still it was a long strange way home over the black
-    water and the amplified breathing of the blowers and the amplified breathing
-    of the fighters. Case felt the edge of the console in faded pinks and
-    yellows. He’d waited in the dark, curled in his jacket pocket. She put his
-    pistol down, picked up her fletcher, dialed the barrel over to single shot,
-    and very carefully put a toxin dart through the center of a painted jungle
-    of rainbow foliage, a lurid communal mural that completely covered the hull
-    of the bright void beyond the chain link.
-  </p>
+  <Tabs {items} {activeItem} on:tabChange={handleTabChange} />
+  {#if activeItem === 'Current Polls'}
+    <p>Poll List</p>
+  {:else if activeItem === 'Add New Poll'}
+    <p>New poll form component goes here</p>
+  {/if}
 </main>
 <Footer />
---
+++ src/shared/Tabs.svelte
@@ -0,0 +1,42 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+
+  export let activeItem;
+  export let items;
+</script>
+
+<style>
+  .active {
+    color: #d91231;
+    border-bottom: 2px solid #d91231;
+    padding-bottom: 8px;
+  }
+
+  .tabs {
+    margin-bottom: 40px;
+  }
+
+  ul {
+    display: flex;
+    justify-content: center;
+    padding: 0;
+    list-style-type: none;
+  }
+  li {
+    margin: 0 16px;
+    font-size: 18px;
+    color: #555;
+    cursor: pointer;
+  }
+</style>
+
+<div class="tabs">
+  <ul>
+    {#each items as item}
+      <li on:click={() => dispatch('tabChange', item)}>
+        <div class:active={item === activeItem}>{item}</div>
+      </li>
+    {/each}
+  </ul>
+</div>
